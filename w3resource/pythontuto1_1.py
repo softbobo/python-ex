26259a569002ffae8136093bbcdba9d087030bec
Robert Schulze <robert_schulze@posteo.de>
2018-08-03 15:22:19 +0200

    first vers, first file - y does python print tabs w/o tab marker?

---
+++ w3resource/pythontuto1_1.py
@@ -0,0 +1,10 @@
+#python basic excercise 1.1
+#soft_bobo, 030818
+#successful 030818
+
+print """Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are"""
